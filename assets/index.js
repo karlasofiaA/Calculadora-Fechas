e8e03780c6b48e47b2da1a788b49b032a91d0abd
Karla Arenas <karlasofia.arenasv@gmail.com>
2023-01-19 20:04:43 -0600

    feat: Condiciones año bisiesto y días de los meses

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,7 @@ console.log("Inicio práctica Calendario");
 const resultado = document.querySelector('#resultado');
 
 //Leemos los valores de entrada dia / mes / año
-valores = () => {
+const valores = () => {
     let dia = parseInt(document.getElementById("dia").value);
     let mes = parseInt(document.getElementById("mes").value);
     let anio = parseInt(document.getElementById("anio").value);
@@ -10,41 +10,52 @@ valores = () => {
     return numeros;
 }
 
-//Función onclick
-function calcular() {
-    validar();
-}
-
 //Comprobar valores numéricos
 function esNumerico(value) {
     return !isNaN(parseFloat(value)) && isFinite(value);
 }
 
+//Comprobar año bisiesto
+const esBisiesto = (c) => {
+    return (c % 400 === 0) ? true :
+        (c % 100 === 0) ? false :
+            (c % 4 === 0) ? true : false
+};
+
+//Función onclick
+function calcular() {
+    validar();
+}
+
 //Validar fecha instroducida
 validar = () => {
     let [a, b, c] = valores();
+    console.log(a+" "+ b +" "+ c);
     let bandera = false;
     let fecha;
     if (esNumerico(a) && esNumerico(b) && esNumerico(c)) {
-        if (a <= 31) { //Meses con 31 días
-            if (b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12)
+        if (a <= 31 && b == 1 || b == 3 || b == 5 || b == 7 || b == 8 || b == 10 || b == 12) { //Meses con 31 días
                 bandera = true;
-        } else if (a <= 30) { //Meses con 30 días
-            if (b == 4 || b == 6 || b == 9 || b == 11)
+        } else if (a <= 30 && b == 4 || b == 6 || b == 9 || b == 11) { //Meses con 30 días
                 bandera = true;
-        } else if (a <= 28) { //Febrero
-            bandera = true;
+        } else if (a <= 29 && b==2) { //Febrero
+            esBisiesto(c) ? bandera = true : 
+                                a <= 28 ? bandera = true : bandera = false;           
         }
         else bandera = false;
     }
+    else {
+        resultado.innerHTML = "-"
+    }
     fecha = new Date(Number(c), Number(b - 1), Number(a))
     bandera == true ? dias(fecha) : resultado.innerHTML = "Fecha invalida"
+    //bandera = false; [a, b, c] = [0, 0, 0];
 }
 
 //Determinar el día de la semana
 function dias(fecha) {
     let diaSemana = fecha.getUTCDay();
-    console.log(diaSemana)
+    //console.log(diaSemana)
     switch (diaSemana) {
         case 0:
             resultado.innerHTML = "Domingo, fin de semana"; break;
@@ -59,6 +70,8 @@ function dias(fecha) {
         case 5:
             resultado.innerHTML = "Viernes, día laboral"; break;
         default:
-            resultado.innerHTML = "Sábado, día laboral"; break;
+            resultado.innerHTML = "Sábado, fin de semana"; break;
     }
 }
+
+
